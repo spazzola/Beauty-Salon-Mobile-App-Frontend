382be1f241df1722ccc87bcfd9be0946d53a6b26
Rafal Szczotka <rafalszczotka@wp.pl>
2021-10-14 20:14:14 +0100

    Added custom icons

---
+++ src/icons/Icons.js
@@ -0,0 +1,21 @@
+import brwi from './brwi.png';
+import manicure from './manicure.png';
+import pedicure from './pedicure.png';
+
+export default [
+    {
+        label: 'Brwi',
+        name: 'brwi',
+        uri: brwi
+    },
+    {
+        label: 'Manicure',
+        name: 'manicure',
+        uri: manicure
+    },
+    {
+        label: 'Pedicure',
+        name: 'pedicure',
+        uri: pedicure
+    }
+]
