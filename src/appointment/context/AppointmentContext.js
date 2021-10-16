e9a7f2a24356b9bbfc48571ef14e2e30f9fd8b66
Rafal Szczotka <rafalszczotka@wp.pl>
2021-10-17 00:37:10 +0100

    Added Appointment Context and API

---
+++ src/appointment/context/AppointmentContext.js
@@ -0,0 +1,105 @@
+import createDataContext from '../../../createDataContext';
+import axios from './AppointmentApi';
+import { format } from 'date-fns'
+
+const appointmentReducer = (state, action) => {
+    switch (action.type) {
+        case 'get_appointments':
+            return action.payload;
+        case 'delete_appointment':
+            return state.filter(appointment => appointment.id !== action.payload);
+        case 'edit_appointment':
+            return state.map(appointment => {
+                return appointment.id === action.payload.id ? action.payload : appointment;
+            });
+        default:
+            return state;
+    }
+}
+
+const addAppointment = dispatch => {
+    return async (startDate, percentageValueToAdd, clientId, employeeId, workIds, callback) => {
+        const formattedDate = format(startDate, 'dd.MM.yyyy HH:mm').replace(/\./g, '/');
+        startDate = formattedDate.substring(0, 10) + " " + formattedDate.substring(11, 16);
+        console.log(workIds);
+        let appointment = {
+            startDate,
+            percentageValueToAdd,
+            clientId,
+            employeeId,
+            workIds
+        };
+        await axios.post('/appointment/create', appointment);
+
+        if (callback) {
+            callback();
+        };
+    };
+}
+const getAppointments = dispatch => {
+    return async () => {
+        const response = await axios.get('/appointment/getAll');
+        dispatch({ type: 'get_appointments', payload: response.data });
+    };
+};
+
+const deleteAppointment = dispatch => {
+    return async id => {
+        await axios.delete('/appointment/delete', {
+            params: {
+                id
+            }
+        });
+        getAppointments();
+        //dispatch({ type: 'delete_client', payload: id });
+    };
+};
+
+const editAppointment = dispatch => {
+    return async (id, name, surname, phoneNumber, callback) => {
+        let appointment = {
+            id,
+            name,
+            surname,
+            phoneNumber
+        };
+        await axios.put('appointment/update', appointment);
+        dispatch({
+            type: 'edit_appointment',
+            payload: { id, name, surname, phoneNumber }
+        });
+        if (callback) {
+            callback();
+        }
+    };
+};
+
+export const { Context, Provider } = createDataContext(
+    appointmentReducer,
+    { addAppointment, getAppointments, deleteAppointment, editAppointment },
+    [])
+
+// {
+//     "id": 1,
+//     "name": "Mariola",
+//     "surname": "Bonaka",
+//     "phoneNumber": "123456789",
+//     "mail": "m@wp.pl",
+//     "belatedCounter": 0
+// },
+// {
+//     "id": 2,
+//     "name": "Janina",
+//     "surname": "Kowalska",
+//     "phoneNumber": "123456789",
+//     "mail": "m@wp.pl",
+//     "belatedCounter": 0
+// },
+// {
+//     "id": 3,
+//     "name": "Ela",
+//     "surname": "Manama",
+//     "phoneNumber": "123456789",
+//     "mail": "m@wp.pl",
+//     "belatedCounter": 0
+// }
