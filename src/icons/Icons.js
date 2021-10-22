74748577792f12b8088258ed005fe75e66344e79
Rafal Szczotka <rafalszczotka@wp.pl>
2021-10-22 04:26:14 +0100

    Added exporting icons for work and buttons

--- src/icons/Icons.js
+++ src/icons/Icons.js
@@ -1,8 +1,27 @@
-import brwi from './brwi.png';
-import manicure from './manicure.png';
-import pedicure from './pedicure.png';
+// WORK ICONS
+import brwi from './work_icons/brwi.png';
+import manicure from './work_icons/manicure.png';
+import pedicure from './work_icons/pedicure.png';
 
-export default [
+//BUTTON ICONS
+import add_appointment from './button_icons/add_appointment.png';
+import add_client from './button_icons/add_client.png';
+import add_cost from './button_icons/add_cost.png';
+import add_employee from './button_icons/add_employee.png';
+import add_service from './button_icons/add_service.png';
+import cancel_appointment from './button_icons/cancel_appointment.png';
+import change_date from './button_icons/change_date.png';
+import clients from './button_icons/clients.png';
+import delete_icon from './button_icons/delete_icon.png';
+import edit_appointment from './button_icons/edit_appointment.png';
+import edit from './button_icons/edit.png';
+import employees from './button_icons/employees.png';
+import services from './button_icons/services.png';
+import solarium_active from './button_icons/solarium_active.png'
+import solarium from './button_icons/solarium.png';
+
+
+const workIcons = [
     {
         label: 'Brwi',
         name: 'brwi',
@@ -19,3 +38,86 @@ export default [
         uri: pedicure
     }
 ]
+
+const buttonIcons = [
+    {
+        name: 'addAppointment',
+        uri: add_appointment
+    },
+    {
+        name: 'addClient',
+        uri: add_client
+    },
+    {
+        name: 'addCost',
+        uri: add_cost
+    },
+    {
+        name: 'addEmployee',
+        uri: add_employee
+    },
+    {
+        name: 'addService',
+        uri: add_service
+    },
+    {
+        name: 'cancelAppointment',
+        uri: cancel_appointment
+    },
+    {
+        name: 'changeDate',
+        uri: change_date
+    },
+    {
+        name: 'clients',
+        uri: clients
+    },
+    {
+        name: 'deleteIcon',
+        uri: delete_icon
+    },
+    {
+        name: 'editAppointment',
+        uri: edit_appointment
+    },
+    {
+        name: 'edit',
+        uri: edit
+    },
+    {
+        name: 'employees',
+        uri: employees
+    },
+    {
+        name: 'services',
+        uri: services
+    },
+    {
+        name: 'solariumActive',
+        uri: solarium_active
+    },
+    {
+        name: 'solarium',
+        uri: solarium
+    }
+]
+
+export { workIcons, buttonIcons };
+
+// export default [
+//     {
+//         label: 'Brwi',
+//         name: 'brwi',
+//         uri: brwi
+//     },
+//     {
+//         label: 'Manicure',
+//         name: 'manicure',
+//         uri: manicure
+//     },
+//     {
+//         label: 'Pedicure',
+//         name: 'pedicure',
+//         uri: pedicure
+//     }
+// ]
