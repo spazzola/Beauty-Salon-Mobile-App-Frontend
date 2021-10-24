a2b5ac516c31285cc84f179fcb2bf5ee4117f2a1
Rafal Szczotka <rafalszczotka@wp.pl>
2021-10-24 03:27:02 +0100

    Exported basic styles to GlobalStyles.js

---
+++ GlobalStyles.js
@@ -0,0 +1,27 @@
+const globalBackground = {
+    backgroundColor: '#fa94e9'
+};
+
+const itemContainer = {
+    height: 60,
+    width: '100%',
+    borderWidth: 2,
+    marginTop: 15,
+    borderRadius: 20,
+    justifyContent: 'center',
+    shadowColor: '#171717',
+    shadowOffset: { width: 2, height: 4 },
+    shadowOpacity: 0.7,
+    shadowRadius: 3,
+    backgroundColor: '#FF00E4',
+
+};
+
+const itemParagraph = {
+    fontSize: 30,
+    width: '100%',
+    flexDirection: 'row',
+    fontFamily: 'KalamBold'
+};
+
+export { globalBackground, itemContainer, itemParagraph };
