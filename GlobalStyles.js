e0afd874b3f4aea912e348c533b9459e345f6b1a
Rafal Szczotka <rafalszczotka@wp.pl>
2021-10-29 11:24:57 +0100

    Added buton styles

--- GlobalStyles.js
+++ GlobalStyles.js
@@ -1,19 +1,20 @@
 const globalBackground = {
-    backgroundColor: '#fa94e9'
+    backgroundColor: '#F4F9F9'
 };
 
 const itemContainer = {
     height: 60,
     width: '100%',
-    borderWidth: 2,
-    marginTop: 15,
-    borderRadius: 20,
+    //width: '90%',
+    //borderWidth: 1,
+    //marginTop: 15,
+    //borderRadius: 30,
     justifyContent: 'center',
     shadowColor: '#171717',
     shadowOffset: { width: 2, height: 4 },
     shadowOpacity: 0.7,
     shadowRadius: 3,
-    backgroundColor: '#FF00E4',
+    backgroundColor: '#DDBEBE',
 
 };
 
@@ -21,7 +22,8 @@ const itemParagraph = {
     fontSize: 30,
     width: '100%',
     flexDirection: 'row',
-    fontFamily: 'KalamBold'
+    fontFamily: 'NotoSerif',
+    color: '#F4F9F9'
 };
 
 const input = {
@@ -55,4 +57,29 @@ const detailTitle = {
     textAlign: 'right'
 }
 
-export { globalBackground, itemContainer, itemParagraph, input, detailTitle, detailParagraph };
+const buttonWrapper = {
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    marginTop: 30,
+    shadowColor: '#171717',
+    shadowOffset: { width: 2, height: 4 },
+    shadowOpacity: 0.7,
+    shadowRadius: 3
+}
+
+const button = {
+    backgroundColor: '#F875AA',
+    width: 160,
+    height: 50,
+    borderRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center'
+}
+
+const buttonText = {
+    fontSize: 20,
+    textAlign: 'center',
+    color: '#F4F9F9'
+}
+
+export { globalBackground, itemContainer, itemParagraph, input, detailTitle, detailParagraph, button, buttonText, buttonWrapper };
