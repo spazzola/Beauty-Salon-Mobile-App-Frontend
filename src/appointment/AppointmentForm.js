fc92a315607385f7297333812f43082c8d05224a
Rafal Szczotka <rafalszczotka@wp.pl>
2021-10-29 11:27:32 +0100

    Added to AppointmentForm functionality to handle entry from Edit and Add mode. Added KeyBoardAvoidingView

--- src/appointment/AppointmentForm.js
+++ src/appointment/AppointmentForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView, KeyboardAvoidingView, Platform, Keyboard, TouchableWithoutFeedback } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import DropDownPicker from 'react-native-dropdown-picker'
 import { Context as ClientContext } from '../client/context/ClientContext';
@@ -7,10 +7,10 @@ import { Context as UserContext } from '../user/context/UserContext';
 import { Context as WorkContext } from '../work/context/WorkContext';
 import NumericInput from 'react-native-numeric-input';
 import { buttonIcons } from '../icons/Icons';
-import { detailTitle, globalBackground } from '../../GlobalStyles';
+import { detailTitle, globalBackground, button, buttonText, buttonWrapper } from '../../GlobalStyles';
 
 
-const AppointmentForm = ({ onSubmit, initialValues, navigation }) => {
+const AppointmentForm = ({ onSubmit, initialValues, navigation, givenDate, mode }) => {
   const clientContext = useContext(ClientContext);
   const userContext = useContext(UserContext);
   const workContext = useContext(WorkContext);
@@ -32,11 +32,13 @@ const AppointmentForm = ({ onSubmit, initialValues, navigation }) => {
   }, []);
 
   let selectedDate = navigation.getParam('selectedDate');
-  // let formattedDate = selectedDate.year + "-" + selectedDate.month + "-" + selectedDate.day;
-  // console.log("selected date: " + formattedDate);
-  // console.log("created date: " + new Date(selectedDate.year, selectedDate.month, selectedDate.day));
-  const [startDate, setStartDate] = useState(selectedDate === undefined ? new Date(Date.now()) : new Date(selectedDate.year, selectedDate.month, selectedDate.day));
-  //const [startTime, setStartTime] = useState(new Date(Date.now()));
+
+  const [startDate, setStartDate] = useState(selectedDate === undefined ? 
+    mode === 'edit' ? 
+      givenDate : 
+      new Date(Date.now()) : 
+    new Date(selectedDate.year, selectedDate.month - 1, selectedDate.day));
+
   const [percentageValueToAdd, setPercentageValueToAdd] = useState(initialValues.value);
 
   const [clientDropDownOpen, setClientDropDownOpen] = useState(false);
@@ -62,149 +64,162 @@ const AppointmentForm = ({ onSubmit, initialValues, navigation }) => {
   // };
 
   return (
-    <ScrollView showsVerticalScrollIndicator={false} style={globalBackground}>
-      <View style={{ height: '100%', backgroundColor: globalBackground.backgroundColor, alignItems: 'center' }}>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'position' : 'height'}
+      style={{ flex: 1 }}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ScrollView showsVerticalScrollIndicator={false} style={globalBackground}>
+          <View style={{ height: '100%', backgroundColor: globalBackground.backgroundColor, alignItems: 'center',  justifyContent: 'space-around' }}>
 
-        <Text style={[detailTitle, styles.label, { fontFamily: 'KalamBold', textAlign: 'center' }]}>Data rozpoczęcia:</Text>
-        <View style={{ width: '90%' }}>
-          <DateTimePicker
-            value={startDate}
-            onChange={onChangeDate}
-            display='spinner'
-            is24Hour={true}
-            locale={'pl'}
-            style={{ backgroundColor: globalBackground.backgroundColor }}
-          />
-        </View>
+            <Text style={[detailTitle, styles.label, { fontFamily: 'NotoSerif', textAlign: 'center' }]}>Data rozpoczęcia:</Text>
+            <View style={{ width: '90%' }}>
+              <DateTimePicker
+                value={startDate}
+                onChange={onChangeDate}
+                display='spinner'
+                is24Hour={true}
+                locale={'pl'}
+                style={{ backgroundColor: globalBackground.backgroundColor }}
+              />
+            </View>
 
-        <Text style={[detailTitle, styles.label, { fontFamily: 'KalamBold', textAlign: 'center' }]}>Godzina rozpoczęcia:</Text>
-        <View style={{ width: '70%' }}>
-          <DateTimePicker
-            value={startDate}
-            onChange={onChangeDate}
-            mode='time'
-            display='spinner'
-            is24Hour={true}
-          />
-        </View>
+            <Text style={[detailTitle, styles.label, { fontFamily: 'NotoSerif', textAlign: 'center' }]}>Godzina rozpoczęcia:</Text>
+            <View style={{ width: '70%' }}>
+              <DateTimePicker
+                value={startDate}
+                onChange={onChangeDate}
+                mode='time'
+                display='spinner'
+                is24Hour={true}
+              />
+            </View>
 
-        <View>
-          <DropDownPicker
-            searchable={true}
-            searchPlaceholder="Wyszukaj..."
-            searchContainerStyle={{
-              borderWidth: 1,
-              borderRadius: 6,
-              backgroundColor: '#FF00E4'
-            }}
-            style={[styles.dropDownPicker]}
-            textStyle={{
-              fontFamily: 'KalamBold',
-              textAlign: 'center',
-              width: '50%',
-              fontSize: 20
-            }}
-            dropDownContainerStyle={{
-              backgroundColor: '#ED50F1',
-              width: '80%',
-              borderWidth: 2
-            }}
-            dropDownDirection="TOP"
-            placeholder="Wybierz klienta"
-            open={clientDropDownOpen}
-            value={clientId}
-            items={clientContext.state.map(client => ({ label: `${client.name} ${client.surname}`, value: client.id }))}
-            setOpen={setClientDropDownOpen}
-            setValue={setClientId}
-            setItems={setClients}
-          />
+            <View>
+              <DropDownPicker
+                searchable={true}
+                searchPlaceholder="Wyszukaj..."
+                searchContainerStyle={{
+                  borderWidth: 1,
+                  borderRadius: 6,
+                  backgroundColor: '#F1D1D0'
+                }}
+                style={[styles.dropDownPicker]}
+                textStyle={{
+                  fontFamily: 'NotoSerif',
+                  textAlign: 'center',
+                  width: '50%',
+                  fontSize: 20
+                }}
+                dropDownContainerStyle={{
+                  backgroundColor: '#F1D1D0',
+                  width: '80%',
+                  borderWidth: 2
+                }}
+                dropDownDirection="TOP"
+                placeholder="Wybierz klienta"
+                open={clientDropDownOpen}
+                value={clientId}
+                items={clientContext.state.map(client => ({ label: `${client.name} ${client.surname}`, value: client.id }))}
+                setOpen={setClientDropDownOpen}
+                setValue={setClientId}
+                setItems={setClients}
+              />
 
-          <DropDownPicker
-            searchable={true}
-            searchPlaceholder="Wyszukaj..."
-            searchContainerStyle={{
-              borderWidth: 1,
-              borderRadius: 6,
-              backgroundColor: '#FF00E4'
-            }}
-            style={[styles.dropDownPicker]}
-            textStyle={{
-              fontFamily: 'KalamBold',
-              textAlign: 'center',
-              width: '50%',
-              fontSize: 20
-            }}
-            dropDownContainerStyle={{
-              backgroundColor: '#ED50F1',
-              width: '80%',
-              borderWidth: 2
-            }}
-            dropDownDirection="TOP"
-            placeholder="Wybierz pracownika"
-            open={userDropDownOpen}
-            value={employeeId}
-            items={userContext.state.map(user => ({ label: `${user.name} ${user.surname}`, value: user.id }))}
-            setOpen={setUserDropDownOpen}
-            setValue={setUserId}
-            setItems={setUsers}
-          />
+              <DropDownPicker
+                searchable={true}
+                searchPlaceholder="Wyszukaj..."
+                searchContainerStyle={{
+                  borderWidth: 1,
+                  borderRadius: 6,
+                  backgroundColor: '#F1D1D0'
+                }}
+                style={[styles.dropDownPicker]}
+                textStyle={{
+                  fontFamily: 'NotoSerif',
+                  textAlign: 'center',
+                  width: '50%',
+                  fontSize: 20
+                }}
+                dropDownContainerStyle={{
+                  backgroundColor: '#F1D1D0',
+                  width: '80%',
+                  borderWidth: 2
+                }}
+                dropDownDirection="TOP"
+                placeholder="Wybierz pracownika"
+                open={userDropDownOpen}
+                value={employeeId}
+                items={userContext.state.map(user => ({ label: `${user.name} ${user.surname}`, value: user.id }))}
+                setOpen={setUserDropDownOpen}
+                setValue={setUserId}
+                setItems={setUsers}
+              />
 
-          <DropDownPicker
-            searchable={true}
-            searchPlaceholder="Wyszukaj..."
-            searchContainerStyle={{
-              borderWidth: 1,
-              borderRadius: 6,
-              backgroundColor: '#FF00E4'
-            }}
-            style={[styles.dropDownPicker]}
-            textStyle={{
-              fontFamily: 'KalamBold',
-              fontSize: 20,
-              textAlign: 'center',
-              //width: '50%'
-            }}
-            dropDownContainerStyle={{
-              backgroundColor: '#ED50F1',
-              width: '80%',
-              borderWidth: 2
-            }}
-            dropDownDirection="TOP"
-            placeholder="Wybierz usługę"
-            multiple={true}
-            min={1}
-            mode="BADGE"
-            badgeColors={["#6ECB63", "#FFEDED", "#95DAC1", "#FD6F96", "#FF67E7"]}
-            showBadgeDot={false}
-            open={workDropDownOpen}
-            value={workIds}
-            items={workContext.state.map(work => ({ label: `${work.name} - ${work.price}zł`, value: work.id }))}
-            setOpen={setWorkDropDownOpen}
-            setValue={setWorkId}
-            setItems={setWorks}
-          />
-        </View>
+              <DropDownPicker
+                searchable={true}
+                searchPlaceholder="Wyszukaj..."
+                searchContainerStyle={{
+                  borderWidth: 1,
+                  borderRadius: 6,
+                  backgroundColor: '#F1D1D0'
+                }}
+                style={[styles.dropDownPicker]}
+                textStyle={{
+                  fontFamily: 'NotoSerif',
+                  fontSize: 20,
+                  textAlign: 'center',
+                  //width: '50%'
+                }}
+                dropDownContainerStyle={{
+                  backgroundColor: '#F1D1D0',
+                  width: '80%',
+                  borderWidth: 2
+                }}
+                dropDownDirection="TOP"
+                placeholder="Wybierz usługę"
+                multiple={true}
+                min={1}
+                mode="BADGE"
+                badgeColors={["#6ECB63", "#FFEDED", "#95DAC1", "#FD6F96", "#FF67E7"]}
+                showBadgeDot={false}
+                open={workDropDownOpen}
+                value={workIds}
+                items={workContext.state.map(work => ({ label: `${work.name} - ${work.price}zł`, value: work.id }))}
+                setOpen={setWorkDropDownOpen}
+                setValue={setWorkId}
+                setItems={setWorks}
+              />
+            </View>
 
 
-        <Text style={[detailTitle, styles.label, { fontFamily: 'KalamBold', marginTop: 30 }]}>Kara procentowa (%):</Text>
-        <NumericInput
-          containerStyle={{ marginTop: 10 }}
-          inputStyle={{ fontFamily: 'KalamRegular' }}
-          minValue={0}
-          rounded={true}
-          rightButtonBackgroundColor='#ED50F1'
-          leftButtonBackgroundColor='#FDB9FC'
-          style={styles.input}
-          value={percentageValueToAdd}
-          onChange={text => setPercentageValueToAdd(text)}
-        />
+            <Text style={[detailTitle, styles.label, { fontFamily: 'NotoSerif', marginTop: 30 }]}>Kara procentowa (%):</Text>
+            <NumericInput
+              inputStyle={{ fontFamily: 'NotoSerif' }}
+              containerStyle={{ marginTop: 10 }}
+              minValue={0}
+              rounded={true}
+              rightButtonBackgroundColor='#FBACCC'
+              leftButtonBackgroundColor='#F1D1D0'
+              style={styles.input}
+              value={percentageValueToAdd}
+              onChange={text => setPercentageValueToAdd(text)}
+            />
 
-        <TouchableOpacity style={{marginBottom: 50, marginTop: 20}} onPress={() => { onSubmit(startDate, percentageValueToAdd, clientId, employeeId, workIds) }}>
-          <Image style={styles.button} source={(buttonIcons.find(icon => icon.name === 'addAppointment')).uri} />
-        </TouchableOpacity>
+            <View style={[buttonWrapper, { marginBottom: 50, marginTop: 30 }]}>
+              <TouchableOpacity style={button} onPress={async () => { 
+                await onSubmit(startDate, percentageValueToAdd, clientId, employeeId, workIds);
+                navigation.navigate('Appointments');
+              }}
+                >
+                <Text style={[buttonText, { fontFamily: 'NotoSerif' }]}>Dodaj wizytę</Text>
+              </TouchableOpacity>
+            </View>
 
-      </View>
-    </ScrollView>
+          </View>
+        </ScrollView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -237,7 +252,7 @@ const styles = StyleSheet.create({
     marginTop: 30,
     width: '80%',
     borderWidth: 2,
-    backgroundColor: '#FF00E4'
+    backgroundColor: '#F1D1D0'
   },
   label: {
     marginTop: 20
