388031a583d099dd0257ec0072fbb0878ac4231f
Rafal Szczotka <rafalszczotka@wp.pl>
2021-11-19 19:04:34 +0000

    Added Signin component with auth logic

---
+++ src/signin/SigninScreen.js
@@ -0,0 +1,75 @@
+import React, { useState, useContext } from 'react';
+import { View, StyleSheet, Text, TouchableOpacity, TextInput, KeyboardAvoidingView, Platform, Keyboard, TouchableWithoutFeedback, } from 'react-native';
+import { Context } from './context/AuthContext';
+import { input, globalBackground, buttonWrapper, button, buttonText } from '../../GlobalStyles';
+import { useFonts } from 'expo-font';
+
+const SigninScreen = ({ navigation }) => {
+    const { state, signin } = useContext(Context);
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [loaded] = useFonts({
+        MerriWeather: require('../../assets/fonts/Merriweather-Regular.ttf'),
+        MerriWeatherBold: require('../../assets/fonts/Merriweather-Bold.ttf')
+    });
+
+    if (!loaded) {
+        return null;
+    }
+
+    return <>
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'margin' : 'height'}
+            style={{ flex: 1 }}
+        >
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View style={[styles.container, { globalBackground }]}>
+                    <TextInput
+                        value={login}
+                        onChangeText={newLogin => setLogin(newLogin)}
+                        placeholder={'Login'}
+                        autoCapitalize='none'
+                        style={[input, { marginLeft: 'auto', marginRight: 'auto', fontFamily: 'MerriWeather' }]}
+                    />
+                    <TextInput
+                        value={password}
+                        onChangeText={newPassword => setPassword(newPassword)}
+                        secureTextEntry={true}
+                        autoCapitalize='none'
+                        placeholder={'Hasło'}
+                        style={[input, { marginLeft: 'auto', marginRight: 'auto', fontFamily: 'MerriWeather' }]}
+                    />
+
+                    <View style={buttonWrapper}>
+                        <TouchableOpacity style={[button, { width: 220 }]} onPress={() => {
+                            signin({ login, password });
+                            //navigation.navigate('mainFlow')
+                         }}>
+                            <Text style={[buttonText, { fontFamily: 'MerriWeatherBold' }]}>Zaloguj</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+    </>
+}
+
+// SigninScreen.navigationOptions = () => {
+//     return {
+//         headerShown: false
+//     }
+// }
+
+SigninScreen.navigationOptions = {
+    title: 'Jakies tam'
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: '40%'
+    }
+});
+
+export default SigninScreen;
---
+++ src/signin/context/AuthContext.js
@@ -0,0 +1,31 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import createDataContext from '../../../createDataContext';
+import axios from '../../../axios-config';
+import { navigate } from '../../navigationRef';
+
+const authReducer = (state, action) => {
+    switch (action.type) {
+        case 'signin':
+            return action.payload;
+        default:
+            return state;
+    }
+};
+
+const signin = dispatch => async ({ login, password }) => {
+    try {
+        const response = await axios.post('/user/authenticate',  { login: login, password: password });
+        await AsyncStorage.setItem('jwt', response.data.jwt);
+        dispatch({ type: 'signin', payload: response.data })
+        navigate('mainFlow');
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+
+export const { Provider, Context } = createDataContext(
+    authReducer,
+    { signin },
+    { isSignedIn: false }
+)
