6ff24d5fdc8a62c27c24de41c1af5d05c083f0b0
Rafal Szczotka <rafalszczotka@wp.pl>
2022-01-10 12:02:04 +0000

    Added functionality to load clients from user's phone contacts

--- src/client/ClientAdd.js
+++ src/client/ClientAdd.js
@@ -1,14 +1,16 @@
 import React, { useContext, useState } from 'react';
 import * as Contacts from 'expo-contacts';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { Context } from './context/ClientContext';
+import { Context as ClientContext } from './context/ClientContext';
+import { Context as AuthContext } from '../signin/context/AuthContext';
 import ClientForm from './ClientForm';
 import { headerBackgroundColor, headerTitleColor, button, buttonText } from '../../GlobalStyles';
 import BaseSpinner from '../base_components/BaseSpinner';
 
 const ClientAdd = ({ navigation }) => {
-  const { addClient, addClients } = useContext(Context);
+  const { addClient, addClients } = useContext(ClientContext);
   const [showSpinner, setShowSpinner] = useState(false);
+  const authContext = useContext(AuthContext);
 
   return (
     <>
@@ -20,30 +22,33 @@ const ClientAdd = ({ navigation }) => {
         />
       </View>
 
-      <TouchableOpacity style={[styles.wrapper, button, { width: 210 }]} onPress={async () => {
-        const { status } = await Contacts.requestPermissionsAsync();
-        if (status === 'granted') {
-          setShowSpinner(!showSpinner);
-          const { data } = await Contacts.getContactsAsync({});
-          if (data.length > 0) {
-            clients = data.map(contact => {
-              if (contact.firstName !== undefined && contact.lastName !== undefined && contact.phoneNumbers !== undefined) {
-                return {
-                  name: contact.firstName,
-                  surname: contact.lastName,
-                  phoneNumber: contact.phoneNumbers[0].number
-                };
+      {authContext.state.role === 'ADMIN' ?
+          <TouchableOpacity style={[styles.wrapper, button, { width: 210 }]} onPress={async () => {
+            const { status } = await Contacts.requestPermissionsAsync();
+            if (status === 'granted') {
+              setShowSpinner(!showSpinner);
+              const { data } = await Contacts.getContactsAsync({});
+              if (data.length > 0) {
+                clients = data.map(contact => {
+                  if (contact.firstName !== undefined && contact.lastName !== undefined && contact.phoneNumbers !== undefined) {
+                    return {
+                      name: contact.firstName,
+                      surname: contact.lastName,
+                      phoneNumber: contact.phoneNumbers[0].number
+                    };
+                  }
+                });
+                const filteredContacts = clients.filter(client => client !== undefined);
+                const response = await addClients(filteredContacts);
+                navigation.navigate('Clients');
+                setShowSpinner(!showSpinner);
               }
-            });
-            const filteredContacts = clients.filter(client => client !== undefined);
-            const response = await addClients(filteredContacts);
-            navigation.navigate('Clients');
-            setShowSpinner(!showSpinner);
-          }
-        }
-      }}>
-        <Text style={[buttonText, { fontFamily: 'MerriWeatherBold' }]}>Importuj kontakty</Text>
-      </TouchableOpacity>
+            }
+          }}>
+            <Text style={[buttonText, { fontFamily: 'MerriWeatherBold' }]}>Importuj kontakty</Text>
+          </TouchableOpacity>
+         : null
+      }
       {showSpinner ?
         <BaseSpinner />
         : null}
