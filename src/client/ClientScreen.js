f001028a0c79febf7fc3aa7917d3a594fd07d657
Rafal Szczotka <rafalszczotka@wp.pl>
2022-01-10 12:00:53 +0000

    Added loading spinner

--- src/client/ClientScreen.js
+++ src/client/ClientScreen.js
@@ -5,6 +5,7 @@ import { Context as AppointmentContext } from '../appointment/context/Appointmen
 import ClientItem from './ClientItem';
 import { globalBackground, button, buttonText, headerBackgroundColor, headerTitleColor } from '../../GlobalStyles';
 import SearchBar from './utility/SearchBar';
+import BaseSpinner from '../base_components/BaseSpinner';
 import { useFonts } from 'expo-font';
 import { buttonIcons } from '../icons/Icons';
 
@@ -18,7 +19,7 @@ function filterList(clients, name) {
         clients.filter(client => {
             return client.name.includes(name) || client.surname.includes(name);
         }).map(filteredClient => resultList.push(filteredClient));
-        
+
         return resultList;
     }
 }
@@ -27,6 +28,7 @@ const ClientScreen = ({ navigation }) => {
     const { state, addClient, getClients } = useContext(Context);
     const appointmentContext = useContext(AppointmentContext);
     const [name, setName] = useState('')
+    const [showSpinner, setShowSpinner] = useState(false);
 
     useEffect(() => {
         getClients();
@@ -44,51 +46,54 @@ const ClientScreen = ({ navigation }) => {
     }, [name])
 
     return (
-        <View style={[globalBackground, styles.container]}>
-            <SearchBar
-                term={name}
-                onTermChange={newName => setName(newName)}
-            />
-            <FlatList
-                showsVerticalScrollIndicator={false}
-                data={filterList(state, name).sort((a, b) => {
-                    let fa = a.name.toLowerCase(),
-                        fb = b.name.toLowerCase(),
-                        fasurname = a.surname.toLowerCase(),
-                        fbsurname = b.surname.toLowerCase();
+        <>
+            <View style={[globalBackground, styles.container]}>
+                <SearchBar
+                    term={name}
+                    onTermChange={newName => setName(newName)}
+                />
+                <FlatList
+                    showsVerticalScrollIndicator={false}
+                    data={filterList(state, name).sort((a, b) => {
+                        let fa = a.name.toLowerCase(),
+                            fb = b.name.toLowerCase(),
+                            fasurname = a.surname.toLowerCase(),
+                            fbsurname = b.surname.toLowerCase();
 
-                    if (fa < fb) {
-                        return -1;
-                    }
-                    if (fa > fb) {
-                        return 1;
-                    }
-                    if (fa < fb || fasurname < fbsurname) {
-                        return -1
-                    }
-                    return 0
-                    //return a.name.localeCompare(b.name); //using String.prototype.localCompare()
-                })}
-                keyExtractor={client => client.id.toString()}
-                renderItem={({ item, index }) => {
-                    return (
-                        <TouchableOpacity onPress={async() => {
-                            //show spinner
-                            response = await appointmentContext.getIncomingAppointments(item.id);
-                            //load appointments
-                            //navigate
-                            //spinner = false
-                            navigation.navigate('Client', { id: item.id })
-                        }}>
-                            <ClientItem client={item} index={index}></ClientItem>
-                        </TouchableOpacity>
-                    );
-                }}
-            />
-            <TouchableOpacity style={[styles.wrapper, button]} onPress={() => navigation.navigate('ClientAdd')}>
-                <Text style={[buttonText, { fontFamily: 'MerriWeatherBold' }]}>Dodaj klienta</Text>
-            </TouchableOpacity>
-        </View>
+                        if (fa < fb) {
+                            return -1;
+                        }
+                        if (fa > fb) {
+                            return 1;
+                        }
+                        if (fa < fb || fasurname < fbsurname) {
+                            return -1
+                        }
+                        return 0
+                        //return a.name.localeCompare(b.name); //using String.prototype.localCompare()
+                    })}
+                    keyExtractor={client => client.id.toString()}
+                    renderItem={({ item, index }) => {
+                        return (
+                            <TouchableOpacity onPress={async () => {
+                                setShowSpinner(!showSpinner);
+                                response = await appointmentContext.getIncomingAppointments(item.id);
+                                navigation.navigate('Client', { id: item.id })
+                                setShowSpinner(false);
+                            }}>
+                                <ClientItem client={item} index={index}></ClientItem>
+                            </TouchableOpacity>
+                        );
+                    }}
+                />
+                <TouchableOpacity style={[styles.wrapper, button]} onPress={() => navigation.navigate('ClientAdd')}>
+                    <Text style={[buttonText, { fontFamily: 'MerriWeatherBold' }]}>Dodaj klienta</Text>
+                </TouchableOpacity>
+            </View>
+            {showSpinner ?
+                <BaseSpinner />
+                : null}
+        </>
     );
 }
 
